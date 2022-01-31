4b93d1cdec79f08b211368f7df1b445e58e34001
Rajshekar Allamsatti <90382338+Rajshekar-2021@users.noreply.github.com>
2022-01-31 14:01:06 +0400

    Update HandTracking.py

--- HandTracking.py
+++ HandTracking.py
@@ -1,7 +1,9 @@
+# Importing necessary libraries
 import cv2
 import mediapipe as mp
 import time
 
+#Capture the video from the web cam
 cap = cv2.VideoCapture(0)
 
 # Setting the camera window to 640,480 size
@@ -14,18 +16,25 @@ mpHands = mp.solutions.hands
 hands = mpHands.Hands()
 mpDraw = mp.solutions.drawing_utils
 
+#Initializing the time, to calculate the Frames per Second (FPS)
 pTime = 0
 
 while True:
     success,img = cap.read()
 
+    # The initial Capture will not create the Mirror Image and hence need to flip the image to create Mirror Image
     #Creating a Mirror Image., and overwritting the initial image
     img = cv2.flip(img,1)
 
+    # The Class mediapipe works with RGB image, while the opencv initially the captures the video in BGR
     #Converting the above BGR image into RGB Image
     imgRGB = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    
+    # Using mediapipe.solutions.hands.Hands()., we need to get the initial co-ordinates in a variable 'result'
     result = hands.process(imgRGB)
     #print(result.multi_hand_landmarks)
+    
+    #Checking the results and if there is any Hand detected the loop starts to draw hand landmarks
     if result.multi_hand_landmarks:
         for handLms in result.multi_hand_landmarks:
             for id,lm in enumerate(handLms.landmark):
