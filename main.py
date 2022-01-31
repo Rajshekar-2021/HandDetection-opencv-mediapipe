fe6f3b4b6a02644c34c06f79d5b101f5662b36c2
Rajshekar Allamsatti <90382338+Rajshekar-2021@users.noreply.github.com>
2022-01-31 06:44:42 +0400

    inserted necessary comments to main.py

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 import cv2
 import mediapipe as mp
 import time
-import HandTrackingModule as htm
+import HandTrackingModule as htm # This is an customized module., and can be used in different projects
 
 cap = cv2.VideoCapture(0)
 detector = htm.handDetector()
 
 # Setting the camera window to 640,480 size
 wCam, hCam = 640, 480
-cap.set(3, wCam)
-cap.set(4, hCam)
-pTime = 0
+cap.set(3, wCam) # 3 here indicates the width of the window
+cap.set(4, hCam) # 4 indicates the height of the window
+pTime = 0        # Initiating the Previous Time to calculate the Frames per second and to display on the window
 
 while True:
     success, img = cap.read()
-    img = detector.findHands(img)
-    lmList = detector.findPosition(img)
+    img = detector.findHands(img) # Uses the customized module and the internal CLASS : handDetector & method : findHands()
+    lmList = detector.findPosition(img) # Uses the customized module and the internal CLASS : handDetector & method : findPosition()
     if len(lmList)!=0:
         print(lmList)
 
